afb4212622df46c8485669a99d0f65ffbef71dd2
Umidjon <umidjongafforov844@gmail.com>
2026-06-02 09:45:48 +0500

    fix

--- src/lib/design.js
+++ src/lib/design.js
@@ -132,7 +132,7 @@ const RUNTIME = `
 })();
 `;
 
-const FONTS = `<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;500;600;700&family=Great+Vibes&family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">`;
+const FONTS = `<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&family=Playfair+Display:ital,wght@0,500;0,600;0,700;1,500;1,600&family=Jost:wght@300;400;500;600&family=Great+Vibes&display=swap" rel="stylesheet">`;
 
 // To'liq, o'zicha ishlaydigan HTML hujjat (iframe srcDoc uchun)
 export function buildPreviewDoc(design, inv) {
